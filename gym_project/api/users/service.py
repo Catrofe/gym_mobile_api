60c8cfb5a45a64feba7501159e7a9351e521fe16
Christian Lopes <christian.lima@ntendencia.com.br>
2023-05-19 16:45:02 -0300

    REFACTOR: Resolvendo variaveis duplicadas.

--- gym_project/api/users/service.py
+++ gym_project/api/users/service.py
@@ -13,15 +13,11 @@ class UserService:
 
     async def register_user(self, user_request: UserRegister) -> UserOutput:
         try:
-            user_valid = await self._repository.user_is_valid(user_request)
             if await self._repository.user_is_valid(user_request):
                 user_request.password = await self.encode_password(user_request.password)
                 user = await self._repository.register_user(user_request)
-                print(user)
-                print(type(user))
-                print(user.email)
-
                 return user, None
+
             return None, status.HTTP_409_CONFLICT
         except Exception as errors:
             return RaiseErrorGym(status.HTTP_500_INTERNAL_SERVER_ERROR, errors)
