417e2c977feafb524f1496ec036322d9d22c1aeb
Christian Lopes de Lima <catrofe@pop-os.localdomain>
2023-05-20 17:14:54 -0300

    REFACTOR: Passando dados sensiveis para variaveis de ambiente.

--- gym_project/utils/settings.py
+++ gym_project/utils/settings.py
@@ -1,9 +1,11 @@
+import os
+
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_test: str = "sqlite+aiosqlite:///db.db"
-    secret_key: str = "h#62yvKrzYjnIc7Cl1Pv%cWK5lkJb8uGTj2u%Phh8%hdnSE6#0"
-    refresh_secret_key: str = "381iIFbg@Y1ndgcPvf*JQh6pq7Nqwb!oF9r3eHU1mPUpusVI9p"
+    db_test: str = str(os.environ.get("GYM_DB_TEST"))
+    secret_key: str = str(os.environ.get("GYM_ACCESS_TOKEN"))
+    refresh_secret_key: str = str(os.environ.get("GYM_REFRESH_TOKEN"))
     secret_expires: int = 7200
     refresh_expires: int = 3600
